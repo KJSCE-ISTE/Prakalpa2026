b51c23fe584fd95d13cf2a349f48f0b78fd4419a
AnamayNarkar <148675323+AnamayNarkar@users.noreply.github.com>
2026-02-11 15:34:38 +0530

    Update vite.config.ts for vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/Prakalpa2026/",
+  plugins: [react()]
+  // base: "/Prakalpa2026/",
 })
