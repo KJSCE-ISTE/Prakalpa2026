0e7cc948e6e4bfcd8be3bbd31ac01da4d583cbea
Harsh0604-2006 <harshmanoj0604@gmail.com>
2026-04-05 17:43:43 +0530

    swapped the text

--- client/src/Stats.tsx
+++ client/src/Stats.tsx
@@ -160,7 +160,7 @@ const GTAStats: React.FC = () => {
       "2px 2px 0px #000, -1px -1px 0px #000, 1px -1px 0px #000, -1px 1px 0px #000",
   }}
 >
-  ENTRY FEES ₹600
+  Funding upto ₹2,00,000
 </div>
  <div
   className="text-sm sm:text-base md:text-lg px-4 py-2"
@@ -171,7 +171,7 @@ const GTAStats: React.FC = () => {
       "2px 2px 0px #000, -1px -1px 0px #000, 1px -1px 0px #000, -1px 1px 0px #000",
   }}
 >
-  Funding upto ₹2,00,000
+ ENTRY FEES ₹600
 </div>
 
       </div>
