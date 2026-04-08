857fccd901e7cc2e42df72ada3a3b67d662e9bce
Harsh0604-2006 <harshmanoj0604@gmail.com>
2026-04-08 19:28:33 +0530

    uncommented

--- server/db.js
+++ server/db.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 // Load environment variables from .env file
 dotenv.config();
 
-// const uri = process.env.MONGO_URI;
+const uri = process.env.MONGO_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
